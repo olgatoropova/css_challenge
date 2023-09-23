b8771d61a12fb1d1f2eb699d73d9f84e76ccbc91
Olga Toropova <fluctuacia@gmail.com>
2023-09-22 20:32:48 -0700

    ApiKey fixed

--- day08_weather_forecast/app.js
+++ day08_weather_forecast/app.js
@@ -1,86 +1,88 @@
 const wrapperElement = document.querySelector('.wrapper');
 
 const daysOfWeekMap = {
-    0: 'SUN',
-    1: 'MON',
-    2: 'TUE',
-    3: 'WED',
-    4: 'THU',
-    5: 'FRI',
-    6: 'SAT'
-}
+	0: 'SUN',
+	1: 'MON',
+	2: 'TUE',
+	3: 'WED',
+	4: 'THU',
+	5: 'FRI',
+	6: 'SAT'
+};
 
 const iconNameToSizeMap = {
-    cloudy: { width: 264, height: 166},
-    sunny: { width: 208, height: 213},
-    stormy: { width: 246, height: 187},
-    snowy: { width: 230, height: 196},
-    rainy: { width: 160, height: 222},
-    'partly-cloudy': { width: 230, height: 209}
-}
+	cloudy: { width: 264, height: 166 },
+	sunny: { width: 208, height: 213 },
+	stormy: { width: 246, height: 187 },
+	snowy: { width: 230, height: 196 },
+	rainy: { width: 160, height: 222 },
+	'partly-cloudy': { width: 230, height: 209 }
+};
 
 const getWeatherTypeByCode = (weatherCode) => {
-    
-    switch (Math.round(weatherCode / 100)) {
-        case 2:
-            return 'stormy';
-        case 3:
-        case 5:
-            return 'rainy';
-        case 6:
-            return 'snowy';
-        case 8:
-            return (weatherCode == 800) ? 'sunny' :
-                (weatherCode == 801) ? 'partly-cloudy' : 'cloudy';
-        default:
-            console.error(`Unknown weather type code ${weatherCode}`);
-            return 'sunny';
-    }
-} 
+	switch (Math.round(weatherCode / 100)) {
+		case 2:
+			return 'stormy';
+		case 3:
+		case 5:
+			return 'rainy';
+		case 6:
+			return 'snowy';
+		case 8:
+			return weatherCode == 800
+				? 'sunny'
+				: weatherCode == 801
+				? 'partly-cloudy'
+				: 'cloudy';
+		default:
+			console.error(`Unknown weather type code ${weatherCode}`);
+			return 'sunny';
+	}
+};
 
 const getWeatherForecast = async () => {
-    try {
-        
-        const options = {
-            method: 'GET',
-            headers: {
-                'X-RapidAPI-Key': '41f6510af3msheec2d84bf994d8ap14c088jsn1422780fb810',
-                'X-RapidAPI-Host': 'weatherbit-v1-mashape.p.rapidapi.com'
-            }
-        };
-        
-        const res = await fetch('https://weatherbit-v1-mashape.p.rapidapi.com/forecast/daily?lat=74.1111&lon=-57.0592', options)
-        const data = await res.json();
-        console.log(data);
-        
-        const retVal = data.data.map(dayWeather => {
-            return {
-                date: dayWeather.valid_date,
-                minTemp: Math.round(dayWeather.min_temp),
-                maxTemp: Math.round(dayWeather.max_temp),
-                pop: dayWeather.pop,
-                weatherType: getWeatherTypeByCode(dayWeather.weather.code)
-            }
-        });
-        console.log(retVal);
-        return retVal;
+	try {
+		const options = {
+			method: 'GET',
+			headers: {
+				'X-RapidAPI-Key': 'API_KEY!!!',
+				'X-RapidAPI-Host': 'weatherbit-v1-mashape.p.rapidapi.com'
+			}
+		};
 
-    } catch (err) {
-        console.error(err);
-    }
-}
+		const res = await fetch(
+			'https://weatherbit-v1-mashape.p.rapidapi.com/forecast/daily?lat=74.1111&lon=-57.0592',
+			options
+		);
+		const data = await res.json();
+		console.log(data);
+
+		const retVal = data.data.map((dayWeather) => {
+			return {
+				date: dayWeather.valid_date,
+				minTemp: Math.round(dayWeather.min_temp),
+				maxTemp: Math.round(dayWeather.max_temp),
+				pop: dayWeather.pop,
+				weatherType: getWeatherTypeByCode(dayWeather.weather.code)
+			};
+		});
+		console.log(retVal);
+		return retVal;
+	} catch (err) {
+		console.error(err);
+	}
+};
 
 const updateUI = (weatherData) => {
-    
-    let weatherHTML = '';
-    for (i = 0; i < 7; i++) {
-        // adding time for correct timezone initialization
-        const date = new Date(weatherData[i].date + 'T12:00');
-        const weatherType = weatherData[i].weatherType;
-        const width = iconNameToSizeMap[weatherType].width;
-        const height = iconNameToSizeMap[weatherType].height;
+	let weatherHTML = '';
+	for (i = 0; i < 7; i++) {
+		// adding time for correct timezone initialization
+		const date = new Date(weatherData[i].date + 'T12:00');
+		const weatherType = weatherData[i].weatherType;
+		const width = iconNameToSizeMap[weatherType].width;
+		const height = iconNameToSizeMap[weatherType].height;
 
-        weatherHTML += `<div class="day ${weatherType}">
+		weatherHTML += `<div class="day ${weatherType}">
             <span class="day-of-week">${daysOfWeekMap[date.getDay()]}</span>
             <span class="date">${date.getDate()}</span>
             <div class="bar">
@@ -108,14 +110,14 @@ const updateUI = (weatherData) => {
                 </div>
             </div>
         </div>`;
-    }
+	}
 
-    wrapperElement.innerHTML = weatherHTML;
-}
+	wrapperElement.innerHTML = weatherHTML;
+};
 
 const loadItUp = async () => {
-    const weatherData = await getWeatherForecast();
-    updateUI(weatherData);
-}
+	const weatherData = await getWeatherForecast();
+	updateUI(weatherData);
+};
 
 loadItUp();
